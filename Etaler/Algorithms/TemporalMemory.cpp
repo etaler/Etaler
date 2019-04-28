1a3a40a2276668cd9f76af76699e09a0c922a7a6
marty1885 <marty188586@gmail.com>
2019-04-29 01:15:15 +0800

    add missing file

---
+++ Etaler/Algorithms/TemporalMemory.cpp
@@ -0,0 +1,39 @@
+#include "TemporalMemory.hpp"
+
+using namespace et;
+
+TemporalMemory::TemporalMemory(const Shape& input_shape, size_t cells_per_column, size_t max_synapses_per_cell, Backend* backend)
+		: cells_per_column_(cells_per_column), backend_(backend)
+{
+	Shape connection_shape = input_shape + cells_per_column + max_synapses_per_cell;
+	std::vector<int32_t> conns(connection_shape.volume(), -1);
+
+	connections_ = backend->createTensor(connection_shape, DType::Int32, conns.data());
+	permances_ = backend->createTensor(connection_shape, DType::Float);
+}
+
+std::pair<Tensor, Tensor> TemporalMemory::compute(const Tensor& x, const Tensor& last_state)
+{
+	Tensor active_cells;
+	if(last_state.has_value() == true)
+		active_cells = backend_->applyBurst(x, last_state);
+	else
+		active_cells = backend_->applyBurst(x, zeros(x.shape()+cells_per_column_, DType::Bool, backend_));
+	Tensor overlap = backend_->overlapScore(active_cells, connections_, permances_, connected_permance_, active_threshold_);
+	Tensor predictive_cells = backend_->cast(overlap, DType::Bool);
+
+	return {predictive_cells, active_cells};
+
+}
+
+void TemporalMemory::learn(const Tensor& active_cells, const Tensor& last_active)
+{
+	if(last_active.has_value() == false)
+		return;
+
+	Tensor learning_cells = backend_->reverseBurst(active_cells);
+
+	backend_->learnCorrilation(last_active, learning_cells, connections_, permances_, permance_inc_, permance_dec_);
+	backend_->growSynapses(last_active, learning_cells, connections_, permances_, 0.21);
+
+}
