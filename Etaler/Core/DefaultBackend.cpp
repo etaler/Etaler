ddba7cbfd4b146a59ea91f7ff014b89c7f747435
marty1885 <marty188586@gmail.com>
2019-05-13 10:29:55 +0800

    holds on shared_ptr when set as default backend

--- Etaler/Core/DefaultBackend.cpp
+++ Etaler/Core/DefaultBackend.cpp
@@ -4,7 +4,7 @@
 
 namespace et
 {
-std::shared_ptr<Backend> g_fallback_backend;
+std::shared_ptr<Backend> g_default_backend_hold;
 Backend* g_default_backend;
 }
 
@@ -12,13 +12,14 @@ using namespace et;
 
 Backend* et::defaultBackend()
 {
+	using DefaultBackendType = CPUBackend;
 	if(g_default_backend == nullptr) {
 		//std::cerr << "Error: defaultBackend() called before setting the default backend.\n";
 		//abort();
-		if((bool)g_fallback_backend == false)
-			g_fallback_backend = std::make_shared<et::CPUBackend>();
+		if((bool)g_default_backend_hold == false)
+			g_default_backend_hold = std::make_shared<DefaultBackendType>();
 
-		g_default_backend = g_fallback_backend.get();
+		g_default_backend = g_default_backend_hold.get();
 	}
 	return g_default_backend;
 }
--- Etaler/Core/DefaultBackend.hpp
+++ Etaler/Core/DefaultBackend.hpp
@@ -8,9 +8,10 @@ namespace et
 {
 
 extern Backend* g_default_backend;
-extern std::shared_ptr<Backend> g_fallback_backend;
+extern std::shared_ptr<Backend> g_default_backend_hold;
+
 inline void setDefaultBackend(Backend* backend) {g_default_backend = backend;}
-inline void setDefaultBackend(std::shared_ptr<Backend> backend) {g_default_backend = backend.get();}
+inline void setDefaultBackend(std::shared_ptr<Backend> backend) {g_default_backend_hold = backend; g_default_backend = backend.get();}
 Backend* defaultBackend();
 
 }
