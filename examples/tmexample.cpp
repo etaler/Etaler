54204c04ec97702c108212fd6e3878d127f5c9df
marty1885 <marty188586@gmail.com>
2019-05-14 14:41:15 +0800

    improve tmexample code

--- examples/tmexample.cpp
+++ examples/tmexample.cpp
@@ -8,6 +8,18 @@ using namespace et;
 #include <iostream>
 #include <numeric>
 
+
+inline std::string to_string(std::vector<size_t> v)
+{
+	if(v.size() == 0)
+		return "None";
+	
+	std::string res = "";
+	for(size_t i=0;i<v.size();i++)
+		res += std::to_string(v[i]) + std::string(i==v.size()-1 ? "" : " ");
+	return res;
+}
+
 int main()
 {
 	//auto backend = std::make_shared<et::OpenCLBackend>();
@@ -22,19 +34,20 @@ int main()
 	for(size_t i=0;i<40;i++) {
 		size_t categoery = i%num_category;
 		Tensor x = encoder::category(categoery, num_category, bits_per_category);
+
 		auto [pred, active] = tm.compute(x, last_state);
+
+		tm.learn(active, last_state); //Let the TM learn
+		last_state = active;
+
+		//Display results
 		auto prediction = sum(pred, 1).cast(DType::Bool);
 		std::vector<size_t> pred_category = decoder::category(prediction, num_category);
 
-		std::cout << "input, prediction of next = " << categoery << ", ";
-		if(pred_category.size() == 0)
-			std::cout << "None";
-		for(auto v : pred_category)
-			std::cout << v << ' ';
+		std::cout << "input, prediction of next = " << categoery
+			<< ", " << to_string(pred_category);
 		std::cout << '\n';
 
-		//Let the TM learn
-		tm.learn(active, last_state);
-		last_state = active;
+		
 	}
 }
