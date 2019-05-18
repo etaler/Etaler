eb4e6d6e48590c72129bb23e2ee059d0de2e56c3
marty1885 <marty188586@gmail.com>
2019-05-19 01:25:19 +0800

    implement SDRClassifer using Tensors

--- Etaler/Algorithms/SDRClassifer.hpp
+++ Etaler/Algorithms/SDRClassifer.hpp
@@ -7,6 +7,8 @@
 namespace et
 {
 
+#if 0
+
 struct SDRClassifer
 {
 	SDRClassifer() = default;
@@ -22,10 +24,7 @@ struct SDRClassifer
 		et_assert(sdr.dtype() == DType::Bool);
 		et_assert(class_id < references_.size());
 
-		auto x = sdr.toHost<uint8_t>();
-		for(size_t i=0;i<references_[class_id].size();i++)
-			references_[class_id][i] += x[i];
-		num_patterns_[class_id] ++;
+		references_[class_id] = references_[class_id] + sdr;
 	}
 
 	size_t compute(const Tensor& x, float min_common_frac=0.75) const
@@ -33,11 +32,7 @@ struct SDRClassifer
 		size_t best_match_id = -1;
 		size_t best_match = 0;
 		for(size_t i=0;i<references_.size();i++) {
-			size_t overlaps = 0;
-			for(size_t j=0;j<references_[i].size();i++) {
-				if(references_[i][j] >= min_common_frac*num_patterns_[i])
-					overlaps++;
-			}
+			size_t overlaps = sum((references_ > num_patterns_*0.75) && x);
 
 			if(overlaps > best_match)
 				std::tie(best_match_id, best_match) = std::pair(i, overlaps);
@@ -46,8 +41,10 @@ struct SDRClassifer
 	}
 
 	Shape input_shape_;
-	std::vector<std::vector<uint32_t>> references_;
+	std::vector<Tensor> references_;
 	std::vector<uint32_t> num_patterns_;
 };
+
+#endif
 
 }
