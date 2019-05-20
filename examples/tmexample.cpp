f3f6bb62a63e9a8b5bb0c60a39a5ccb9e711d95d
marty1885 <marty188586@gmail.com>
2019-05-20 09:50:31 +0800

    remvoe including OpenCLBackend for container build

--- examples/tmexample.cpp
+++ examples/tmexample.cpp
@@ -1,6 +1,6 @@
 #include <Etaler/Etaler.hpp>
 #include <Etaler/Backends/CPUBackend.hpp>
-#include <Etaler/Backends/OpenCLBackend.hpp>
+//#include <Etaler/Backends/OpenCLBackend.hpp>
 #include <Etaler/Algorithms/TemporalMemory.hpp>
 #include <Etaler/Encoders/Category.hpp>
 using namespace et;
