edb4f702c8937a3fe6e8634a8516321821650c2b
marty1885 <marty188586@gmail.com>
2019-05-21 10:53:02 +0800

    build OpenCL tests automatically

--- tests/main_opencl.cpp
+++ tests/main_opencl.cpp
@@ -2,11 +2,14 @@
 #include "catch2/catch.hpp"
 
 
-//#include <Etaler/Backends/CPUBackend.hpp>
+#include <Etaler/Backends/OpenCLBackend.hpp>
 #include <Etaler/Core/DefaultBackend.hpp>
 
 int main( int argc, char* argv[] )
 {
+	auto backend = std::make_shared<et::OpenCLBackend>();
+	et::setDefaultBackend(backend.get());
+
 	std::cout << "Running with backend: " << et::defaultBackend()->name() << std::endl;
 
 	int result = Catch::Session().run( argc, argv );
