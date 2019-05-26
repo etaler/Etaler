8343cc9e86c3e4b9cb8370ebbf51886e0718e406
Lior Assouline <alior101@gmail.com>
2019-05-26 13:44:04 +0000

    wip

--- examples/tmexample.cpp
+++ examples/tmexample.cpp
@@ -49,8 +49,25 @@ int main()
 		std::cout << "input, prediction of next = " << categoery
 			<< ", " << to_string(pred_category);
 		std::cout << '\n';
+	
+		if (i==0)
+		{
+			std::vector<std::vector<double>> x1, y1, z1;
+    		for (double i = -5; i <= 5;  i += 0.25) {
+				std::vector<double> x_row, y_row, z_row;
+				for (double j = -5; j <= 5; j += 0.25) {
+					x_row.push_back(i);
+					y_row.push_back(j);
+					z_row.push_back(::std::sin(::std::hypot(i, j)));
+				}
+				x1.push_back(x_row);
+				y1.push_back(y_row);
+				z1.push_back(z_row);
+			}		
 
-  		plt::plot({1,3,2,4});
-    	plt::show();
+			plt::plot_scatter(x1,y1,z1);;
+			plt::show();
+		}
+		
 	}
 }
