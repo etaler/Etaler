a6734ad386d83cca4e4f220392337abcb1ed9b1c
Lior Assouline <alior101@gmail.com>
2019-07-18 17:48:53 +0300

    modified LayersVisualization location and license

--- examples/visualize/tm_visualize.cpp
+++ examples/visualize/tm_visualize.cpp
@@ -1,6 +1,23 @@
+/*
+tm visualization example - see Etaler TM in action in RT in 3D!
+Copyright (C) 2019 Lior Assouline
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Etaler/Etaler.hpp>
 #include <Etaler/Backends/CPUBackend.hpp>
-//#include <Etaler/Backends/OpenCLBackend.hpp>
 #include <Etaler/Algorithms/TemporalMemory.hpp>
 #include <Etaler/Encoders/Category.hpp>
 using namespace et;
