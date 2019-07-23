67755495a991b8d1ff01e29f41f879d61a45db41
marty1885 <marty188586@gmail.com>
2019-07-23 23:02:45 +0800

    allow tm_visualize also work on the OpenCLBackend

--- examples/visualize/tm_visualize.cpp
+++ examples/visualize/tm_visualize.cpp
@@ -44,7 +44,8 @@ int main(int argc, char **argv)
 	char the_path[256];
 
     getcwd(the_path, 255);
-    printf("Executable is run from %s - please make sure shaders resources are on that path...\n", the_path);	//auto backend = std::make_shared<et::OpenCLBackend>();
+    printf("Executable is run from %s - please make sure shaders resources are on that path...\n", the_path);
+	//auto backend = std::make_shared<et::OpenCLBackend>();
 	//et::setDefaultBackend(backend.get());
 	size_t num_category = 3;
 	size_t bits_per_category = 5;
@@ -55,8 +56,7 @@ int main(int argc, char **argv)
 
 	Tensor last_state = zeros({sdr_size, cells_per_column}, DType::Bool);
 
-	Visualizer * vis;
-	vis = new Visualizer(cells_per_column, bits_per_category*num_category);
+	std::unique_ptr<Visualizer> vis = std::make_unique<Visualizer>(cells_per_column, bits_per_category*num_category);
 
 	for(size_t i=0;i<40;i++) {
 		size_t categoery = i%num_category;
@@ -64,8 +64,8 @@ int main(int argc, char **argv)
 
 		auto [pred, active] = tm.compute(x, last_state);
 
-		bool * active_buff = (bool*)active.data();
-		vis->UpdateLayer(0, active_buff);
+		auto active_buff = active.toHost<uint8_t>();
+		vis->UpdateLayer(0, (bool*)active_buff.data());
 
 		//std::cout << active << std::endl;
 		usleep(1000000);
