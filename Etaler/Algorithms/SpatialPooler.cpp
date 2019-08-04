fb030403335434b84ae6e11f4404a9ac119c7cb2
marty1885 <marty188586@gmail.com>
2019-08-04 21:55:28 +0800

    move SpatialPooler w topology to another class

--- Etaler/Algorithms/SpatialPooler.cpp
+++ Etaler/Algorithms/SpatialPooler.cpp
@@ -52,56 +52,6 @@ SpatialPooler::SpatialPooler(const Shape& input_shape, const Shape& output_shape
 	average_activity_ = constant(output_shape, global_density);
 }
 
-SpatialPooler::SpatialPooler(const Shape& input_shape, size_t kernel_size, size_t stride, float potential_pool_pct, size_t seed
-	, float global_density, float boost_factor, Backend* b)
-	:global_density_(global_density), boost_factor_(boost_factor), input_shape_(input_shape)
-{
-	for(size_t i=0;i<input_shape.size();i++)
-		et_assert(input_shape[i] >= (intmax_t)kernel_size);
-
-	Shape output_shape(input_shape.size());
-	for(size_t i=0;i<input_shape.size();i++)
-		output_shape[i] = (input_shape[i]-kernel_size)/stride+1;
-	output_shape_ = output_shape;
-
-	size_t input_cell_num = input_shape.volume();
-	size_t potential_pool_size = std::max((size_t)(std::pow(kernel_size, input_shape.size())*potential_pool_pct), size_t{1});
-
-	pcg64 rng(seed);
-	std::vector<int> all_input_cell = vector_range<int>(0, input_cell_num);
-	Tensor indices = Tensor({input_shape}, all_input_cell.data(), b);
-	all_input_cell.clear();
-
-	connections_ = Tensor(output_shape+potential_pool_size, DType::Int32, b);
-	permanences_ = Tensor(output_shape+potential_pool_size, DType::Float, b);
-
-	for(size_t i=0;i<(size_t)output_shape.volume();i++) {
-		svector<Range> write_loc;
-		Shape loc = foldIndex(i, output_shape);
-		for(size_t j=0;j<output_shape.size();j++)
-			write_loc.push_back(loc[j]);
-
-		svector<Range> read_loc(loc.size());
-		for(size_t j=0;j<loc.size();j++) {
-			intmax_t pos = loc[j]*stride;
-			read_loc[j] = range(pos, pos+kernel_size);
-		}
-
-		std::vector<int> conns = indices.view(read_loc).toHost<int>();
-		assert(conns.size() >= potential_pool_size);
-		std::shuffle(conns.begin(), conns.end(), rng);
-		std::sort(conns.begin(), conns.begin()+potential_pool_size);
-
-		std::vector<float> permanences(potential_pool_size);
-		std::normal_distribution<float> dist(connected_permanence_, 1);
-		std::generate(permanences.begin(), permanences.end(), [&](){return std::max(std::min(dist(rng), 1.f), 0.f);});
-
-		connections_.view(write_loc) = Tensor({(intmax_t)potential_pool_size}, conns.data());
-		permanences_.view(write_loc) = Tensor({(intmax_t)potential_pool_size}, permanences.data());
-	}
-	average_activity_ = constant(output_shape, global_density);
-}
-
 Tensor SpatialPooler::compute(const Tensor& x) const
 {
 	et_assert(x.shape() == input_shape_);
