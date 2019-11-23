7ac8bfb4e9c7a0e5ddbc63a869f6b44ddf8c2ffc
marty1885 <marty188586@gmail.com>
2019-11-23 21:23:35 +0800

    Fix serialize failing on cling

--- Etaler/Core/Serialize.hpp
+++ Etaler/Core/Serialize.hpp
@@ -4,12 +4,14 @@
 #include <any>
 #include <string>
 
+#include "Etaler_export.h"
+
 namespace et
 {
 
 using StateDict = std::map<std::string, std::any>;
 
-void save(const StateDict& dict, const std::string& path);
-StateDict load(const std::string& path);
+void ETALER_EXPORT save(const StateDict& dict, const std::string& path);
+StateDict ETALER_EXPORT load(const std::string& path);
 
 }
