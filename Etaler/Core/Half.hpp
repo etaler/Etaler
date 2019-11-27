fef02d4cf67964e6907de7f8813b883d0c2a2dea
marty1885 <marty188586@gmail.com>
2019-11-27 16:51:34 +0800

    add printing fp16 via cling

--- Etaler/Core/Half.hpp
+++ Etaler/Core/Half.hpp
@@ -3,11 +3,25 @@
 #include <cstdint>
 #include <Etaler/3rdparty/half_precision/half.hpp>
 
+#include <sstream>
+
 namespace et
 {
 
 using half = half_precision::half;
 
 using float16 = half;
+
+}
+
+namespace cling
+{
+
+inline std::string printValue(const et::half* value)
+{
+	std::stringstream ss;
+	ss << (float)(*value);
+	return ss.str();
+}
 
 }
