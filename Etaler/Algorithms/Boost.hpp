c478d14b41d373067d989797489a85f860c026f1
marty1885 <marty188586@gmail.com>
2019-12-08 15:08:59 +0800

    implement log boosting

--- Etaler/Algorithms/Boost.hpp
+++ Etaler/Algorithms/Boost.hpp
@@ -17,4 +17,9 @@ static Tensor boost(const Tensor& activity, const Tensor& average_activity, floa
 	return cast(boostFactor(average_activity, target_activity, boost_factor)*activity, DType::Int32);
 }
 
+static Tensor logarithmicBoost(const Tensor& activity, const Tensor& average_activity, float target_activity, float target_density, int active_threshold)
+{
+	return activity * (log(average_activity - target_activity)/log(target_density - sum(activity > active_threshold).item<int>()));
+}
+
 }
